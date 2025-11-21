8ffc8cf6d839b7c343f75a7b9ca19a691eb9517e
thomas.polo <thomas.polo@epita.fr>
2025-11-21 23:07:21 +0100

    feat: added readme and setup script for dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-sudo apt install eza vivid curl stow
+sudo apt install curl wget stow build-essential
+
+wget "https://github.com/sharkdp/vivid/releases/download/v0.10.1/vivid_0.10.1_amd64.deb"
+sudo dpkg -i vivid_0.10.1_amd64.deb
+
+sudo apt update
+sudo apt install -y gpg
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
